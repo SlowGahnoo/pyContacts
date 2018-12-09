ca913f7d16bca45f5f9f09a0aad419de7d90a42d
SlowGahnoo <45499650+SlowGahnoo@users.noreply.github.com>
2018-12-09 21:10:12 +0200

    Add files via upload

--- pycon.py
+++ pycon.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
-
-import kivy
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 
-class ContactGrid(Widget):
+class ScreenManager(ScreenManager):
     pass
+
+class MenuScreen(Screen):
+    pass
+
+class ContactList(Widget):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+class AboutScreen(Screen):
+    pass
+
+buildKV = Builder.load_file("my.kv")
+
 
 class MyApp(App):
 
     def build(self):
-        return ContactGrid()
-
-
-
-
-
-if __name__=="__main__" :
-    MyApp().run()
+        return buildKV 
+    
+if __name__=='__main__':
+    pycon=MyApp()
+    pycon.run()
