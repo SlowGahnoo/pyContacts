5302713021edae49510ed7488c0002adc0223a95
SlowGahnoo <45499650+SlowGahnoo@users.noreply.github.com>
2018-12-18 21:30:33 +0200

    Add files via upload

--- pycon.py
+++ pycon.py
@@ -4,10 +4,22 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 
+class TopBar(Widget):
+    pass
+
+class BottomBar(Widget):
+    pass
+
+class ReturnBar(Widget):
+    pass
+
 class ScreenManager(ScreenManager):
     pass
 
 class MenuScreen(Screen):
+    pass
+
+class AddContactScreen(Screen):
     pass
 
 class ContactList(Widget):
