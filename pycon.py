82b4a7229f2524075396d4394759c6ce2647daa3
SlowGahnoo <45499650+SlowGahnoo@users.noreply.github.com>
2018-12-28 15:16:33 +0200

    Add files via upload

--- pycon.py
+++ pycon.py
@@ -3,11 +3,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.listview import ListItemButton
+
 
 class TopBar(Widget):
-    pass
-
-class BottomBar(Widget):
     pass
 
 class ReturnBar(Widget):
@@ -20,10 +21,21 @@ class MenuScreen(Screen):
     pass
 
 class AddContactScreen(Screen):
+    
+    first_name_text_input=ObjectProperty()
+    last_name_text_input=ObjectProperty()
+    
+    def add_contact(self):
+        contact_name = self.first_name_text_input.text+" "+self.last_name_text_input.text
+        ContactList.contact_list.data.extend([contact_name])
+
+        ContactList.contact_list._trigger_reset_populate()
+
+class ContactListButton(ListItemButton):
     pass
 
 class ContactList(Widget):
-    pass
+    contact_list=ObjectProperty()
 
 class SettingsScreen(Screen):
     pass
