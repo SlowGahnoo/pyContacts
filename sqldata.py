ec9db7ebe348e98152f0b53bd8e9ad83a6c0f572
SlowGahnoo <45499650+SlowGahnoo@users.noreply.github.com>
2019-01-08 14:31:56 +0200

    Update sqldata.py

--- sqldata.py
+++ sqldata.py
@@ -4,16 +4,13 @@ import sqlite3
 
 connection =sqlite3.connect('database.db')
 cursor = connection.cursor()
-try:
-    cursor.execute('CREATE TABLE names (id INTEGER PRIMARY KEY, name TEXT, surname TEXT)')
+try: cursor.execute('CREATE TABLE names (id INTEGER PRIMARY KEY, name TEXT, surname TEXT)')
 except: pass
-total=1
 while True: 
     cursor.execute('SELECT * FROM names')
     rows = cursor.fetchall()
     for row in rows:
-        total=len(rows)+1
-        print('id: '+str(row[0])+'  name:'+str(row[1])+' surname: '+str(row[2]))
+        print('name:'+str(row[1])+' surname: '+str(row[2]))
     print('''What would you like to do?
     
        press  1 to add
@@ -29,8 +26,8 @@ while True:
         connection.commit()
     if usr == "2":
         try:
-            usr=input('which id?')
-            cursor.execute('DELETE FROM names WHERE id LIKE "{}"'.format(usr))
+            usr=input('who? (Enter first name)')
+            cursor.execute('DELETE FROM names WHERE name LIKE "{}"'.format(usr))
             connection.commit()
         except: print('Error')
 
