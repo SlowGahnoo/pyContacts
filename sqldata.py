d096fea731248c952d07ad0857520bd41349b35a
manos1324 <45516655+manos1324@users.noreply.github.com>
2019-01-10 12:37:49 +0200

    Update sqldata.py

--- sqldata.py
+++ sqldata.py
@@ -1,37 +1,67 @@
 #!/usr/bin/env python3
-
+import Epafes_importable as Ep
 import sqlite3
 
-connection=sqlite3.connect('database.db')
+connection=sqlite3.connect('Contacts.db')
 cursor = connection.cursor()
-try: cursor.execute('CREATE TABLE names (name TEXT, surname TEXT)')
+try: cursor.execute('CREATE TABLE IF NOT EXISTS Contacts (name TEXT, surname TEXT, Mobile INTEGER, Home INTEGER, Office INTEGER)')
 except: pass
 while True: 
-    cursor.execute('SELECT * FROM names')
+    cursor.execute('SELECT * FROM Contacts')
     rows = cursor.fetchall()
     for row in rows:
         print('name:'+str(row[0])+' surname: '+str(row[1]))
-    print('''What would you like to do?
-    
-       press  1 to add
-       press  2 to remove 
-       
-        ''')
+    print('What would you like to do?\n\n1->Add a contact\n2->Remove a contact\n3->Add or change something to a contact\n4->exit')
     usrInput=input()
+    
     if usrInput == "1":
         try:
-            usrName,usrSurname=input().split(' ')
+            
+            usrName=input('Name:')
+            usrSurname=input('Surname:')
+            usrMobile=input('Mobile:')
+            usrHome=input('Home:')
+            usrOffice=input('Office:')
+                
+            if usrName=='':
+                print('Your contact has to have a Name')
+            else:
+                cursor.execute('SELECT * FROM Contacts WHERE name="{}" AND surname="{}"'.format(usrName,usrSurname))
+                matrix=cursor.fetchall()
+                
+                if matrix!=[]:
+                    print('There is already a contact with this name')
+                
+                else:
+                    cursor.execute('INSERT INTO Contacts VALUES(?,?,?,?,?)',(usrName, usrSurname,usrMobile,usrHome,usrOffice))
+                    connection.commit()
+                    
         except: pass
-        cursor.execute('INSERT INTO names VALUES(?, ?)',(usrName, usrSurname))
-        connection.commit()
+        
     if usrInput == "2":
         try:
             usrREMOVE=input('who? (Enter first name)')
-            #Has to be fixed as it removes people that share the same name
-            cursor.execute('DELETE FROM names WHERE name LIKE "{}"'.format(usrREMOVE))
+            cursor.execute('DELETE FROM Contacts WHERE name LIKE "{}"'.format(usrREMOVE))
             connection.commit()
         except: print('Error')
-
-
-
-
+        
+    if usrInput == "3":
+        usrName=input('What is the name of the contact?')
+        usrSurname=input('What is the surname of the contact?')
+        cursor.execute('SELECT * FROM Contacts WHERE name="{}" AND surname="{}"'.format(usrName,usrSurname))
+        matrix=cursor.fetchall()
+        if matrix == []:
+            print('This contact does not exist')
+            
+        else:
+            usrKind=input('What do you want to add or change?(Mobile,Home,Office)')
+            if not (usrKind=='Mobile' or usrKind=='Home' or usrKind=='Office'):
+                print('This is not a valid kind of number')
+            else:
+                usrNumber=input('Type the number you want to add')
+##                cursor.execute('INSERT INTO Contacts (Home) WHERE name="{}" AND surname="{}" VALUES(?)'.format(usrKind,usrName,usrSurname),(usrNumber))
+                cursor.execute('UPDATE Contacts SET ("{}"=?) WHERE name LIKE "{}" AND surname LIKE"{}" '.format(usrKind,usrName,usrSurname)),(usrNumber)
+                connection.commit()
+        
+    if usrInput=="4":
+        break
